5685f690fb3c6f558bfcf1226d47d8ab78c91815
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-09-27 12:58:17 +0300

    Update recipe carousel with responsive cards

--- src/app/components/CarouselRecipe.tsx
+++ src/app/components/CarouselRecipe.tsx
@@ -10,6 +10,7 @@ interface Props {
 import * as React from "react"
 import useEmblaCarousel from "embla-carousel-react"
 import Image from "next/image"
+import { RecipeDialog } from "./RecipeDialog"
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -38,18 +39,17 @@ export default function CarouselRecipe(props: Props) {
   }, [emblaApi])
 
   return (
-    <Carousel className="w-[90%] mx-auto border border-gray-200 dark:border-gray-600 rounded-xl">
+    <Carousel className="w-[80%] mx-auto border border-gray-200 dark:border-gray-600 rounded-xl">
       <CarouselContent ref={emblaRef}>
         {recipes.map((recipe: Recipe, index: number) => (
-          <CarouselItem key={index} className="basis-1/3 sm:basis-1/4 md:basis-1/5 lg:basis-1/6">
+          <CarouselItem key={index} className="basis-1/2 sm:basis-1/2 md:basis-1/3 lg:basis-1/4">
             <div className="">
               <Card className="w-full h-full">
                 <CardContent className="flex items-center justify-center p-2 lg:p-4">
-                  {/* <div className="relative"> */}
                     {/* <Image src={recipe.recipe.image} alt={recipe.recipe.label} width={300} height={300}/> */}
-                    <Image src="/images/sample-img.jpg" alt="sample" width={300} height={300}
-                      className="w-full h-full object-cover rounded-lg md:rounded-xl" />
-                  {/* </div> */}
+                    {/* <Image src="/images/sample-img.jpg" alt="sample" width={300} height={300}
+                      className="w-full h-full object-cover rounded-lg md:rounded-xl" /> */}
+                    <RecipeDialog recipe={recipe} />
                 </CardContent>
               </Card>
             </div>

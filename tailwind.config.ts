0f87a8898147355072c57325aafc4613d164c6c9
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-09-30 22:20:32 +0300

    Fix ts issue and variable declaration issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColo
 import svgToDataUri from 'mini-svg-data-uri'
 
 function addVariablesForColors({ addBase, theme }: any) {
-	let allColors = flattenColorPalette(theme('colors'))
-	let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]))
+	const allColors = flattenColorPalette(theme('colors'))
+	const newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]))
 
 	addBase({
 		':root': newVars
@@ -89,7 +89,7 @@ const config: Config = {
 		function ({ matchUtilities, theme, addUtilities }: any) {
 		  matchUtilities(
 			{
-			  'bg-grid': (value: any) => ({
+			  'bg-grid': (value: string) => ({
 				backgroundImage: `url("${svgToDataUri(
 				  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
 				)}")`
